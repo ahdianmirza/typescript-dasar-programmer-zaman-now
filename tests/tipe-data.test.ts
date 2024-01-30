b2ac12a4f5e0a141ed444bbd1eb3b329f3a7d69f
ahdianmirza <ahdianmirza@gmail.com>
2024-01-30 12:06:55 +0700

    Babel Typescript

--- tests/tipe-data.test.ts
+++ tests/tipe-data.test.ts
@@ -1,11 +1,15 @@
 describe('Data Type', function () {
     it('should must declare', function () {
-        const name: string = "Ahdian Mirza Azri";
-        const balance: number = 10000;
-        const isVip: boolean = true;
+        let name: string = "Ahdian Mirza Azri";
+        let balance: number = 10000;
+        let isVip: boolean = true;
 
         console.info(name);
         console.info(balance);
         console.info(isVip);
+
+        // name = 100; //error
+        // balance = "ahdian"; //error
+        // isVip = 100; //error
     })
 })
