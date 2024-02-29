618c3047394bd2bc3cd74209b6fb9eb667be03ca
ahdianmirza <ahdianmirza@gmail.com>
2024-02-29 09:42:08 +0700

    Function Interface

--- tests/interface.test.ts
+++ tests/interface.test.ts
@@ -11,5 +11,17 @@ describe ('Interface', () => {
 
         seller.name = "Toko Ahdian",
         console.info(seller);
+    });
+
+    it ('should support function interface', () => {
+        interface AddFunction {
+            (value1: number, value2: number): number
+        }
+
+        const add: AddFunction = (value1: number, value2: number): number => {
+            return value1 + value2;
+        }
+
+        expect(add(2,2)).toBe(4);
     })
 })
