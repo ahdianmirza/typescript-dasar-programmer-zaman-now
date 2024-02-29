188d8d7f04f733d314c18dbe11b1cd799d9d8b77
ahdianmirza <ahdianmirza@gmail.com>
2024-02-29 08:57:12 +0700

    Object Type

---
+++ tests/object.test.ts
@@ -0,0 +1,14 @@
+describe ('Object', () => {
+    it ('should support in typescript', () => {
+        const person: {id: string, name: string} = {
+            id: "1",
+            name: "Ahdian"
+        };
+
+        console.info("Person: ", person);
+
+        person.id = "2";
+        person.name = "Mirza"
+        console.info("Person: ", person);
+    })
+})
