277b24769f13b789c2b82f2f9e4574148ced5e20
ahdianmirza <ahdianmirza@gmail.com>
2024-02-29 08:50:26 +0700

    Type Alias

---
+++ tests/type-alias.test.ts
@@ -0,0 +1,20 @@
+import { Category, Product } from "../src/type-alias"
+
+describe ("Type Alias", () => {
+    it ("should support in typescript", () => {
+        const category: Category = {
+            id: 1,
+            name: "Handphone"
+        }
+
+        const product: Product = {
+            id: "1",
+            name: "Samsung",
+            price: 2000000,
+            category: category
+        }
+
+        console.info("Category: ", category);
+        console.info("Product: ", product);
+    })
+})
